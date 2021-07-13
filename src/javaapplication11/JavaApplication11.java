5a7d4c6fedb15690ba035724e5f7aeabb1268c32
ishanka158 <96714@fhss.sjp.ac.lk>
2021-07-13 11:22:28 +0530

    secondd

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -15,6 +15,7 @@ public class JavaApplication11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello");
         // TODO code application logic here
     }
     
